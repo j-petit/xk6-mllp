df8f1210dcffdb6c3b7b9dc9c069d8f173ad41c8
Jens Petit <petit.jens@gmail.com>
2021-03-17 11:40:49 +0100

    adapt

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 import mllp from 'k6/x/mllp';
 
 const client = new mllp.Client({
-    host: '127.0.0.1',
-    port: '5000'
+    host: 'clio.cloud.aiqnet.org',
+    port: '443'
 });
 
 export default function () {
